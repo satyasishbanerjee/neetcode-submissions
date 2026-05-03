5b9036dd8459472806cb6b499fd6c4f6a64d4251
satyasishbanerjee <154687602+satyasishbanerjee@users.noreply.github.com>
2026-05-03 19:20:55 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 11
+float_type = 2.09
+bool_type = False
+string_type = "yo"
+list_type = [2,3,"sex"]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
