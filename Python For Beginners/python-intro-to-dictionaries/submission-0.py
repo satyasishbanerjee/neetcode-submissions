575965dd64723ad7caf9747568dd6b621eaaeaad
satyasishbanerjee <154687602+satyasishbanerjee@users.noreply.github.com>
2026-05-07 16:17:01 +0530

    Add: python-intro-to-dictionaries - submission-0

---
+++ Python For Beginners/python-intro-to-dictionaries/submission-0.py
@@ -0,0 +1,20 @@
+from typing import List, Dict
+
+def create_dict(name: str, age: int) -> Dict[str, int]:
+    return {name:age}
+
+
+def list_to_dict(words: List[str]) -> Dict[str, int]:
+    dic = {}
+    for i in range(len(words)):
+        dic[words[i]] = i 
+    return dic
+
+
+# don't modify code below this line
+print(create_dict("Alice", 25))
+print(create_dict("Jane", 35))
+print(create_dict("Joe", 45))
+
+print(list_to_dict(["Alice", "Jane", "Joe"]))
+print(list_to_dict(["Apple", "Banana", "Watermelon", "Pineapple"]))
